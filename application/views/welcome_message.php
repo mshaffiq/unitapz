22037e854c7590031253251beaac8fcced4cbc53
mshaffiq <mshaffiqhussin@gmail.com>
2023-05-02 15:47:32 +0800

    adjust font

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -27,44 +27,44 @@ defined('BASEPATH') or exit('No direct script access allowed');
 <body>
   <div class="paper container">
     <h1>Pinger Demo</h1>
-    <h4>Manual check tekkis server uptime</h4>
+    <h3>Manual check tekkis server uptime</h3>
   </div>
   <div class="paper container" style="margin-top:1rem;">
     <button class="btn-success" id="checkServerBtn">Check!</button>
     <div class="card" style="width: 100%;margin-top:1rem;">
       <div class="card-header">t-Auth API (tauthapi.tekkis.my)</div>
       <div class="card-body">
-        <h4 class="card-title">Server is <span class="upText text-success">UP!</span></h4>
+        <h3 class="card-title">Server is <span class="upText text-success">UP!</span></h3>
       </div>
     </div>
     <div class="card" style="width: 100%;margin-top:1rem;">
       <div class="card-header">t-Pay API (api.tpay.com.my)</div>
       <div class="card-body">
-        <h4 class="card-title">Server is <span class="upText text-success">UP!</span></h4>
+        <h3 class="card-title">Server is <span class="upText text-success">UP!</span></h3>
       </div>
     </div>
     <div class="card" style="width: 100%;margin-top:1rem;">
       <div class="card-header">t-Auth Admin (tauth-staging.tekkis.my)</div>
       <div class="card-body">
-        <h4 class="card-title">Server is <span class="upText text-success">UP!</span></h4>
+        <h3 class="card-title">Server is <span class="upText text-success">UP!</span></h3>
       </div>
     </div>
     <div class="card" style="width: 100%;margin-top:1rem;">
       <div class="card-header">t-Verify Admin (merchant-staging.tverify.com.my)</div>
       <div class="card-body">
-        <h4 class="card-title">Server is <span class="upText text-success">UP!</span></h4>
+        <h3 class="card-title">Server is <span class="upText text-success">UP!</span></h3>
       </div>
     </div>
     <div class="card" style="width: 100%;margin-top:1rem;">
       <div class="card-header">t-Pay Portal (merchant-staging.tpay.com.my)</div>
       <div class="card-body">
-        <h4 class="card-title">Server is <span class="upText text-success">UP!</span></h4>
+        <h3 class="card-title">Server is <span class="upText text-success">UP!</span></h3>
       </div>
     </div>
     <div class="card" style="width: 100%;margin-top:1rem;">
       <div class="card-header">t-Pay Checkout (checkout-staging.tpay.com.my)</div>
       <div class="card-body">
-        <h4 class="card-title">Server is <span class="upText text-success">UP!</span></h4>
+        <h3 class="card-title">Server is <span class="upText text-success">UP!</span></h3>
       </div>
     </div>
   </div>
