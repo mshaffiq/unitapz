02a3020fd7afa208207a21e0cdc112a38c4e8863
mshaffiq <mshaffiqhussin@gmail.com>
2023-05-02 15:45:18 +0800

    added jquery and buttons

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,17 +1,73 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
+defined('BASEPATH') or exit('No direct script access allowed');
+?>
 
-	<link rel="stylesheet" href="<?php echo base_url("assets/papercss/css/paper.min.css") ?>">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script>
+  $(document).ready(function() {
+    $("#checkServerBtn").click(function() {
+      $(".upText").html("...");
+      setTimeout(function() {
+        $(".upText").html("UP!");
+      }, 2000);
+    });
+  });
+</script>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <title>Welcome to CodeIgniter</title>
+
+  <link rel="stylesheet" href="<?php echo base_url("assets/papercss/css/paper.min.css") ?>">
 </head>
-  <body>
-    <div class="paper container">
-      <h1>Some Fresh Title</h1>
-      <p>This is where some content would go.</p>
+
+<body>
+  <div class="paper container">
+    <h1>Pinger Demo</h1>
+    <h4>Manual check tekkis server uptime</h4>
+  </div>
+  <div class="paper container" style="margin-top:1rem;">
+    <button class="btn-success" id="checkServerBtn">Check!</button>
+    <div class="card" style="width: 100%;margin-top:1rem;">
+      <div class="card-header">t-Auth API (tauthapi.tekkis.my)</div>
+      <div class="card-body">
+        <h4 class="card-title">Server is <span class="upText text-success">UP!</span></h4>
+      </div>
     </div>
-  </body>
+    <div class="card" style="width: 100%;margin-top:1rem;">
+      <div class="card-header">t-Pay API (api.tpay.com.my)</div>
+      <div class="card-body">
+        <h4 class="card-title">Server is <span class="upText text-success">UP!</span></h4>
+      </div>
+    </div>
+    <div class="card" style="width: 100%;margin-top:1rem;">
+      <div class="card-header">t-Auth Admin (tauth-staging.tekkis.my)</div>
+      <div class="card-body">
+        <h4 class="card-title">Server is <span class="upText text-success">UP!</span></h4>
+      </div>
+    </div>
+    <div class="card" style="width: 100%;margin-top:1rem;">
+      <div class="card-header">t-Verify Admin (merchant-staging.tverify.com.my)</div>
+      <div class="card-body">
+        <h4 class="card-title">Server is <span class="upText text-success">UP!</span></h4>
+      </div>
+    </div>
+    <div class="card" style="width: 100%;margin-top:1rem;">
+      <div class="card-header">t-Pay Portal (merchant-staging.tpay.com.my)</div>
+      <div class="card-body">
+        <h4 class="card-title">Server is <span class="upText text-success">UP!</span></h4>
+      </div>
+    </div>
+    <div class="card" style="width: 100%;margin-top:1rem;">
+      <div class="card-header">t-Pay Checkout (checkout-staging.tpay.com.my)</div>
+      <div class="card-body">
+        <h4 class="card-title">Server is <span class="upText text-success">UP!</span></h4>
+      </div>
+    </div>
+  </div>
+</body>
+
 </html>
